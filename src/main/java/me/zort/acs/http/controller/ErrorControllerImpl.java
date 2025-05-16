8dc110679495191a630a0520a62b1ad9e728852d
ZorTik <zortlegit@gmail.com>
2025-05-16 21:56:27 +0200

    ErrorControllerImpl

--- src/main/java/me/zort/acs/http/controller/ErrorControllerImpl.java
+++ src/main/java/me/zort/acs/http/controller/ErrorControllerImpl.java
@@ -3,6 +3,7 @@ package me.zort.acs.http.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import me.zort.acs.http.dto.body.BasicResponse;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,7 +22,9 @@ public class ErrorControllerImpl extends AbstractErrorController {
     @RequestMapping(value = "/error", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BasicResponse> handleUnknownRequest(HttpServletRequest request) {
         HttpStatus status = getStatus(request);
+        String message = (String) getErrorAttributes(request, ErrorAttributeOptions.defaults())
+                .getOrDefault("message", "Unknown error");
 
-        return ResponseEntity.status(status).body(new BasicResponse(status.value(), "No message"));
+        return ResponseEntity.status(status).body(new BasicResponse(status.value(), message));
     }
 }
