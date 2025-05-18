5dccbb4648fefe8d5869b77f0a8bf7fda4818cce
ZorTik <zortlegit@gmail.com>
2025-05-18 18:12:33 +0200

    Javadoc

--- src/main/java/me/zort/acs/domain/access/validator/AccessRequestValidator.java
+++ src/main/java/me/zort/acs/domain/access/validator/AccessRequestValidator.java
@@ -4,6 +4,9 @@ import me.zort.acs.domain.model.Node;
 import me.zort.acs.domain.model.SubjectLike;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Validates an AccessRequest object if it's allowed to be even checked for applicability.
+ */
 public interface AccessRequestValidator {
 
     @Nullable
