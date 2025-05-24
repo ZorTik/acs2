e305b83f42623874bf2defebc7217a09277485b9
ZorTik <zortlegit@gmail.com>
2025-05-25 00:58:35 +0200

    Purpose of Disposable

--- acs-server/src/main/java/me/zort/acs/domain/garbage/Disposable.java
+++ acs-server/src/main/java/me/zort/acs/domain/garbage/Disposable.java
@@ -1,4 +1,7 @@
 package me.zort.acs.domain.garbage;
 
 public interface Disposable {
+    // Marks an object as disposable. Purpose of this is to
+    // prevent unintentional use of non-disposable objects in the
+    // ResourceDisposalRegistry.
 }
