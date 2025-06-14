5e2f0a37e62535f469963f2322beed75192f345b
ZorTik <zortlegit@gmail.com>
2025-06-14 13:27:27 +0200

    RealmService#deleteRealm

--- acs-plane/src/main/java/me/zort/acs_plane/domain/realm/RealmServiceImpl.java
+++ acs-plane/src/main/java/me/zort/acs_plane/domain/realm/RealmServiceImpl.java
@@ -3,9 +3,12 @@ package me.zort.acs_plane.domain.realm;
 import me.zort.acs_plane.api.domain.realm.Realm;
 import me.zort.acs_plane.api.domain.realm.RealmService;
 import me.zort.acs_plane.api.domain.realm.exception.RealmAlreadyExistsException;
+import me.zort.acs_plane.api.domain.realm.exception.RealmNotExistsException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class RealmServiceImpl implements RealmService {
     // TODO: link realmů objektů který nastaví existing na false pokud přijde delete event
 
@@ -13,6 +16,13 @@ public class RealmServiceImpl implements RealmService {
     public Realm createRealm(String realm) throws RealmAlreadyExistsException {
         // TODO
         return null;
+    }
+
+    @Override
+    public void deleteRealm(Realm realm) throws RealmNotExistsException {
+        realm.requireExists();
+
+        // TODO
     }
 
     @Override
