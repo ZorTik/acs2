d7d9c3f8cb3335b4c4e46a8a6d998c8c045ff3dd
ZorTik <zortlegit@gmail.com>
2025-06-15 12:17:33 +0200

    plane: definitions endpoint and mappers

---
+++ acs-plane/src/main/java/me/zort/acs_plane/api/http/mapper/HttpRealmMapper.java
@@ -0,0 +1,10 @@
+package me.zort.acs_plane.api.http.mapper;
+
+import me.zort.acs_plane.api.domain.realm.Realm;
+
+/**
+ * Maps HTTP request data to a {@link Realm} object.
+ * The data here represent Realm's name.
+ */
+public interface HttpRealmMapper extends HttpToDomainMapper<String, Realm> {
+}
