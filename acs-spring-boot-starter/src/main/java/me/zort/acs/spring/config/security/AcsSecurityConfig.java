e914e64528740337e28477cddfe67621c8150e1b
ZorTik <zortlegit@gmail.com>
2025-06-16 17:26:45 +0200

    README changes, SystemSubjectProviderFactory

--- acs-spring-boot-starter/src/main/java/me/zort/acs/spring/config/security/AcsSecurityConfig.java
+++ acs-spring-boot-starter/src/main/java/me/zort/acs/spring/config/security/AcsSecurityConfig.java
@@ -1,11 +1,9 @@
 package me.zort.acs.spring.config.security;
 
-import me.zort.acs.core.domain.SubjectByTypeAndIdProvider;
 import me.zort.acs.core.domain.SubjectProvider;
 import me.zort.acs.client.v1.AcsClientV1;
 import me.zort.acs.spring.*;
-import me.zort.acs.spring.config.model.SubjectConfig;
-import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -22,18 +20,15 @@ public class AcsSecurityConfig {
     public AcsUserDetailsService userDetailsService(
             AcsClientV1 client,
             AcsSecuritySystemMatcherConfig systemMatcherConfig,
-            AcsSecurityUserConfig userConfig, AcsSecurityConfigurer securityConfigurer) {
-        return securityConfigurer.userDetailsService(
-                client,
-                pickSubjectProviderForConfig(systemMatcherConfig), userConfig.getSubjectType());
-    }
-
-    private static @NotNull SubjectProvider pickSubjectProviderForConfig(SubjectConfig config) {
-        if (config.getSubjectType() != null && config.getSubjectId() != null) {
-            return new SubjectByTypeAndIdProvider(config.getSubjectType(), config.getSubjectId());
-        } else {
-            throw new InvalidAcsConfigurationException("Invalid subject config! You probably forgot " +
-                    "to set the right combination of properties.");
+            AcsSecurityUserConfig userConfig,
+            AcsSecurityConfigurer securityConfigurer,
+            @Autowired(required = false) SystemSubjectProviderFactory systemSubjectProviderFactory) {
+        if (systemSubjectProviderFactory == null) {
+            systemSubjectProviderFactory = new DefaultSystemSubjectProviderFactory();
         }
+
+        SubjectProvider systemSubjectProvider = systemSubjectProviderFactory
+                .createSystemSubjectProvider(systemMatcherConfig);
+        return securityConfigurer.userDetailsService(client, systemSubjectProvider, userConfig.getSubjectType());
     }
 }
