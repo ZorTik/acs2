39898c9e436cb07251fc0a553b44a7c45c9b4e72
ZorTik <zortlegit@gmail.com>
2025-06-16 15:46:23 +0200

    Format validation for subject request params

--- acs-server/src/main/java/me/zort/acs/http/internal/resolvers/SubjectArgumentResolver.java
+++ acs-server/src/main/java/me/zort/acs/http/internal/resolvers/SubjectArgumentResolver.java
@@ -8,14 +8,29 @@ import me.zort.acs.http.internal.annotation.SubjectRequestParam;
 import me.zort.acs.http.mapper.HttpSubjectMapper;
 import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+/**
+ * Argument resolver for controller method parameters annotated with
+ * {@link me.zort.acs.http.internal.annotation.SubjectRequestParam}.
+ * <p>
+ * This resolver extracts and deserializes subject-related request parameters
+ * and maps them to domain objects using the provided {@link HttpSubjectMapper}.
+ * </p>
+ * <p>
+ * Supports parameters of type {@link me.zort.acs.api.domain.model.SubjectLike}
+ * and {@link me.zort.acs.domain.model.Subject}.
+ * </p>
+ */
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 @Component
 public class SubjectArgumentResolver implements HandlerMethodArgumentResolver {
@@ -31,9 +46,10 @@ public class SubjectArgumentResolver implements HandlerMethodArgumentResolver {
                 && ArrayUtils.contains(SUPPORTED_PARAMETER_TYPES, parameter.getParameterType());
     }
 
+    @Nullable
     private String[] deserializeValue(String serializedValue) {
         if (!serializedValue.contains(":")) {
-            // TODO: Throw
+            return null;
         }
 
         return serializedValue.split(":", 2);
@@ -50,13 +66,22 @@ public class SubjectArgumentResolver implements HandlerMethodArgumentResolver {
         String serializedValue = webRequest.getParameter(annot.value());
         if (serializedValue == null) {
             if (annot.required()) {
-                // TODO: Throw
+                // Missing query parameter, but is required
+                throw new MissingServletRequestParameterException(
+                        annot.value(), parameter.getParameterType().getSimpleName());
             }
 
             return null;
         }
 
         String[] values = deserializeValue(serializedValue);
+
+        if (values == null) {
+            Throwable cause = new IllegalArgumentException("Invalid format for parameter: expected 'group:id'");
+
+            throw new MethodArgumentTypeMismatchException(
+                    serializedValue, String.class, annot.value(), parameter, cause);
+        }
 
         return subjectMapper.toDomainOrNull(SubjectDto.builder()
                 .group(values[0])
