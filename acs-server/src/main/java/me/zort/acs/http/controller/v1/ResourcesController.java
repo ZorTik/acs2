7b5e64beab646df555b9265c9ebb5a4c45783a90
ZorTik <zortlegit@gmail.com>
2025-06-16 18:28:52 +0200

    Subject argument resolver update, init subjects list ep

---
+++ acs-server/src/main/java/me/zort/acs/http/controller/v1/ResourcesController.java
@@ -0,0 +1,25 @@
+package me.zort.acs.http.controller.v1;
+
+import lombok.RequiredArgsConstructor;
+import me.zort.acs.domain.model.Subject;
+import me.zort.acs.http.dto.body.subjects.list.ListSubjectsResponseDto;
+import me.zort.acs.http.internal.annotation.SubjectRequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@RequestMapping("/v1")
+@RestController
+public class ResourcesController {
+
+    @GetMapping("/resources/granted")
+    public ListSubjectsResponseDto grantedByHolders(
+            @SubjectRequestParam("accessor") Subject accessor,
+            @RequestParam String subjectType, @RequestParam String[] groups, @RequestParam String[] nodes) {
+        // TODO
+        return null;
+    }
+}
