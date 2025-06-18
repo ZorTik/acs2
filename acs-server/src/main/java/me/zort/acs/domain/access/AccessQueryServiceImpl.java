693357bb0ec07e7cccc81327cb177ca6727549c8
ZorTik <zortlegit@gmail.com>
2025-06-18 15:05:18 +0200

    Refactor query service

--- acs-server/src/main/java/me/zort/acs/domain/access/AccessQueryServiceImpl.java
+++ acs-server/src/main/java/me/zort/acs/domain/access/AccessQueryServiceImpl.java
@@ -2,6 +2,7 @@ package me.zort.acs.domain.access;
 
 import lombok.RequiredArgsConstructor;
 import me.zort.acs.api.domain.access.AccessQueryService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -18,49 +19,93 @@ import java.util.function.BiFunction;
 public class AccessQueryServiceImpl implements AccessQueryService {
     private final List<AccessQueryable> sources;
 
+    /**
+     * Performs a partial query across multiple queryables.
+     *
+     * @param queryFunction The function to query the data from the queryable.
+     * @param pageable The requested pageable object.
+     * @param aggregatedQueryableOffset Cumulative offset of all previous queryables.
+     * @param cache A cache to store the results as they are fetched.
+     * @param queryable The current queryable (source) being processed.
+     * @param lookupPage The page fetched from the current queryable to determine the total elements and offset.
+     * @return A page containing the results from the queryables.
+     * @param <T> The type of the elements in the page.
+     */
+    private <T> @NotNull Page<T> queryPartially(
+            BiFunction<AccessQueryable, Pageable, Page<T>> queryFunction,
+            Pageable pageable,
+            long aggregatedQueryableOffset, List<T> cache, AccessQueryable queryable, Page<T> lookupPage) {
+        final long requestedOffset = pageable.getOffset();
+        final long totalElementsInSource = lookupPage.getTotalElements();
+
+        long localOffset = requestedOffset - aggregatedQueryableOffset;
+        int remaining = pageable.getPageSize();
+
+        PageRequest partialPageable = PageRequest.of((int) (localOffset / pageable.getPageSize()), remaining);
+        Page<T> partialPage = queryFunction.apply(queryable, partialPageable);
+
+        cache.addAll(partialPage.getContent());
+        remaining -= partialPage.getContent().size();
+
+        // Continue collecting from next queryables
+        for (int j = sources.indexOf(queryable) + 1; j < sources.size() && remaining > 0; j++) {
+            AccessQueryable nextQueryable = sources.get(j);
+            Page<T> nextPage = queryFunction.apply(nextQueryable, PageRequest.of(0, remaining));
+            cache.addAll(nextPage.getContent());
+            remaining -= nextPage.getContent().size();
+        }
+
+        return new PageImpl<>(cache, pageable, totalElementsInSource + lookupPage.getTotalElements());
+    }
+
+    /**
+     * Queries the data fully from a single queryable.
+     * This is used when the requested page is fully contained within a single queryable.
+     *
+     * @param queryFunction The function to query the data from the queryable.
+     * @param pageable The requested pageable object.
+     * @param queryable The queryable (source) to query from.
+     * @param aggregatedQueryableOffset Cumulative offset of all previous queryables.
+     * @return A page containing the results from the queryable.
+     * @param <T> The type of the elements in the page.
+     */
+    private static <T> Page<T> queryFully(
+            BiFunction<AccessQueryable, Pageable, Page<T>> queryFunction,
+            Pageable pageable, AccessQueryable queryable, long aggregatedQueryableOffset) {
+        final long requestedOffset = pageable.getOffset();
+
+        long pageOffset = requestedOffset - aggregatedQueryableOffset;
+        int pageSize = pageable.getPageSize();
+
+        return queryFunction.apply(queryable, PageRequest.of((int) (pageOffset / pageSize), pageSize));
+    }
+
+    /**
+     * @see AccessQueryService#performAggregatedQuery(BiFunction, Pageable)
+     */
     @Override
     public <T> Page<T> performAggregatedQuery(
             BiFunction<AccessQueryable, Pageable, Page<T>> queryFunction, Pageable pageable) {
-        long requestedOffset = pageable.getOffset();
+        final long requestedOffset = pageable.getOffset();
 
         List<T> cache = new ArrayList<>(pageable.getPageSize());
         long aggregatedQueryableOffset = 0;
         for (AccessQueryable queryable : sources) {
             Page<T> lookupPage = queryFunction.apply(queryable, PageRequest.of(0, 1));
 
-            long totalElementsInSource = lookupPage.getTotalElements();
+            final long totalElementsInSource = lookupPage.getTotalElements();
             if (requestedOffset >= aggregatedQueryableOffset
                     && requestedOffset + pageable.getPageSize() < aggregatedQueryableOffset + totalElementsInSource) {
                 // If the current offset is within the range of this queryable and the page is fully in the source,
                 // we can fetch the page from it.
-                long pageOffset = requestedOffset - aggregatedQueryableOffset;
-                int pageSize = pageable.getPageSize();
-
-                return queryFunction.apply(queryable, PageRequest.of((int) (pageOffset / pageSize), pageSize));
+                return queryFully(queryFunction, pageable, queryable, aggregatedQueryableOffset);
             } else if (requestedOffset >= aggregatedQueryableOffset
                     // The page starts in this queryable
                     && requestedOffset < aggregatedQueryableOffset + totalElementsInSource
                     && requestedOffset + pageable.getPageSize() >= aggregatedQueryableOffset + totalElementsInSource) {
                 // Page is larger than the source, so we need to fetch the partial page.
 
-                long localOffset = requestedOffset - aggregatedQueryableOffset;
-                int remaining = pageable.getPageSize();
-
-                PageRequest partialPageable = PageRequest.of((int) (localOffset / pageable.getPageSize()), remaining);
-                Page<T> partialPage = queryFunction.apply(queryable, partialPageable);
-
-                cache.addAll(partialPage.getContent());
-                remaining -= partialPage.getContent().size();
-
-                // Continue collecting from next queryables
-                for (int j = sources.indexOf(queryable) + 1; j < sources.size() && remaining > 0; j++) {
-                    AccessQueryable nextQueryable = sources.get(j);
-                    Page<T> nextPage = queryFunction.apply(nextQueryable, PageRequest.of(0, remaining));
-                    cache.addAll(nextPage.getContent());
-                    remaining -= nextPage.getContent().size();
-                }
-
-                return new PageImpl<>(cache, pageable, totalElementsInSource + lookupPage.getTotalElements());
+                return queryPartially(queryFunction, pageable, aggregatedQueryableOffset, cache, queryable, lookupPage);
             }
 
             aggregatedQueryableOffset += totalElementsInSource;
