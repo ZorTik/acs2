3013dd4185a5889d9f83aa8158f2de382257d89f
ZorTik <zortlegit@gmail.com>
2025-06-19 02:18:08 +0200

    Javadocs

--- acs-server/src/main/java/me/zort/acs/api/data/service/PersistenceEntityProvider.java
+++ acs-server/src/main/java/me/zort/acs/api/data/service/PersistenceEntityProvider.java
@@ -1,6 +1,21 @@
 package me.zort.acs.api.data.service;
 
+/**
+ * Interface for providing persistence entities, supporting caching.
+ * <p>
+ * Implementations should return a cached entity if available,
+ * or create and cache a new one if not present.
+ * </p>
+ */
 public interface PersistenceEntityProvider {
 
+    /**
+     * Returns a cached entity of the given class and ID, or creates and caches a new one if not present.
+     *
+     * @param entityClass the class of the entity
+     * @param id the identifier of the entity
+     * @param <T> the type of the entity
+     * @return the cached or newly created entity
+     */
     <T> T getCachedOrCreate(Class<T> entityClass, Object id);
 }
